9419f37415ba06545f5ed75f54d8b253259ca080
Liza <lizapikovskaya@gmail.com>
2023-04-06 12:07:24 +0300

    feat: do task 1 and 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,41 +1,23 @@
 import Player from '@vimeo/player';
+import _ from 'lodash';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-console.log(player);
-// player.on('timeupdate', function (data) {
-//   const localStorage = {
-//     'videoplayer-current-time': data.seconds,
-//   };
-// });
-
-
-// player
-//   .setCurrentTime()
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
-
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
-
+const currentTimeVideo = 'videoplayer-current-time';
 
 player.on(
-  'timeupdate',function (e) {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
-    console.log();
-  });
-// player
-//   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-//   .catch(function (error) {
-//     console.error(error);
-//   });
+  'timeupdate',
+  _.throttle(function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem(currentTimeVideo, currentTime);
+  }),
+  1000
+);
+
+
+player
+  .setCurrentTime(localStorage.getItem(currentTimeVideo))
+  .catch(function (error) {});
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import _ from 'lodash';
 
+
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', onFormInput)
+// formEl.addEventListener('submit', onFormSubmit);
+
+
+const inputEmailEl = document.querySelector('input[name="email"]');
+const textareaMessageEl = document.querySelector('textarea[name="email"]');
+const FEEDBACK_FORM_STATE = "feedback-form-state";
+const formData = {}
+
+function onFormInput (event) {
+formData[event.target.name] = event.target.value
+localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));
+
+}
+
+function restoreFormValue() {
+const savedData = localStorage.getItem(JSON.parse(FEEDBACK_FORM_STATE));
+
+if(savedData) {
+inputEmailEl.value = savedData[inputEmailEl.name];
+textareaMessageEl.value = savedData[textareaMessageEl.name];
+}
+}
+
+restoreFormValue()
+
+// function onFormSubmit(event) {
+// event.preventDefault()
+// if(!event.target.trim()) {
+//     alert ('Заповніть поля, будь ласка');
+//     return
+// }
+// }
+
+// if (window.localStorage) {
+//   var elements = document.querySelectorAll('[name]');
+
+//   for (var i = 0, length = elements.length; i < length; i++) {
+//     (function (element) {
+//       var name = element.getAttribute('name');
+
+//       element.value = localStorage.getItem(name) || '';
+
+//       element.onkeyup = function () {
+//         localStorage.setItem(name, element.value);
+//       };
+//     })(elements[i]);
+//   }
+// }
